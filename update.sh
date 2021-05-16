63404cd0e4e6627b9fe615f7da18704e80df6ab1
Cook1e <vincent.schweiger@xolley.de>
2021-05-16 17:40:50 +0200

    updated packages

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 dpkg-scanpackages -m ./debs > Packages
-rm Packages.bz2
-bzip2 Packages
-
+rm Packages{.xz,.gz,.bz2,.zst}
+gzip -c9 Packages > Packages.gz
+xz -c9 Packages > Packages.xz
+zstd -c19 Packages > Packages.zst
+bzip2 -c9 Packages > Packages.bz2
+echo "Done updating packages!"
